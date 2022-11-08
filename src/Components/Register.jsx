4a574fbbc75ac2b9df2e831bb8aa0d5583d648c8
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 14:56:59 +0200

    update

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -6,7 +6,8 @@ export default function Register() {
           <div className=' w-75 mx-auto'>
               <h2>Register Now</h2>
           <form>
-              
+                  <label htmlFor="first_name">First Name:</label>
+                  
         </form>
 
           </div>
