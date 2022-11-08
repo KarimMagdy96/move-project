e61fa094addb93834acbd162eaceb7acce8a085f
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 14:55:35 +0200

    update

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -4,8 +4,8 @@ export default function Register() {
   return (
       <>
           <div className=' w-75 mx-auto'>
-              
-          <form action="">
+              <h2>Register Now</h2>
+          <form>
               
         </form>
 
