bc95ce4ad53d4e9f8a73c4e1b0aaa0c9065e7faf
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 13:44:27 +0200

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
       <>
-   <nav className="navbar navbar-expand-lg bg-light">
+   <nav className="navbar navbar-expand-lg bg-transparent">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
