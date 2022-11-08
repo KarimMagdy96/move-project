83cfb64d54d0c014f4ad4380e86a64bad3d4090a
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 14:58:07 +0200

    update

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 export default function Register() {
   return (
-      <>
-          <div className=' w-75 mx-auto'>
-              <h2>Register Now</h2>
-          <form>
-                  <label htmlFor="first_name">First Name:</label>
-                  
+    <>
+      <div className=" w-75 mx-auto">
+        <h2>Register Now</h2>
+        <form>
+          <label htmlFor="first_name">First Name:</label>
+          <input type="text" id="first_name" name="first_name" className=' form-control' />
         </form>
-
-          </div>
+      </div>
     </>
-  )
+  );
 }
