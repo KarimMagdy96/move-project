bcabdf2532969adc00f9a37e06c9af07d0c2ac68
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-09 00:57:00 +0200

    update

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,11 +1,14 @@
 import Axios from "axios";
 import Joi from "joi";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
     const [error, seterror] = useState("");
     const [errorList, seterrorList] = useState([]);
     const [isLodaing, setIsLoading] = useState(false);
     const [succuss, setsuccuss] = useState("");
+    let navigate = useNavigate();
     const [user, setusers] = useState({
         first_name: "",
         last_name: "",
@@ -24,6 +27,7 @@ export default function Register() {
         let validInput = validateRegisterForm();
         if (validInput.error) {
             seterrorList(validInput.error.details);
+            setIsLoading(false);
         } else {
             let { data } = await Axios.post(
                 "https://route-egypt-api.herokuapp.com/signup",
@@ -31,17 +35,20 @@ export default function Register() {
             );
             if (data.message === "success") {
                 setsuccuss(data.message);
+                setIsLoading(false);
+                navigate("/login");
                 //nav to home
             } else {
                 seterror(data.message);
+                setIsLoading(false);
             }
         }
     }
     function validateRegisterForm() {
         let validator = Joi.object({
             first_name: Joi.string().alphanum().min(3).max(30).required(),
-            last_name: Joi.string().alphanum().min(16).max(80).required(),
-            age: Joi.number().min(3).max(30).required(),
+            last_name: Joi.string().alphanum().min(3).max(30).required(),
+            age: Joi.number().min(16).max(80).required(),
             email: Joi.string()
                 .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
                 .required(),
@@ -52,69 +59,73 @@ export default function Register() {
         return validator.validate(user, { abortEarly: false });
     }
     return (
-        <>
-            <div className=" w-75 mx-auto mt-3">
-                <h2 className="mb-5">Register Now</h2>
+      <>
+        <div className=" w-75 mx-auto mt-3">
+          <h2 className="mb-5">Register Now</h2>
 
-                <form onSubmit={sumbitForm}>
-                    {errorList.map((error) => (
-                        <div className=" alert alert-danger">{error.message}</div>
-                    ))}
-                    {error.length > 0 ? (
-                        <div className=" alert alert-danger">{error}</div>
-                    ) : succuss.length > 0 ? (
-                        <div className=" alert alert-success">{succuss}</div>
-                    ) : (
-                        ""
-                    )}
-                    <label htmlFor="first_name">First Name:</label>
-                    <input
-                        onChange={getUserData}
-                        type="text"
-                        id="first_name"
-                        name="first_name"
-                        className=" form-control mb-2"
-                    />
+          <form onSubmit={sumbitForm}>
+            {errorList.map((error) => (
+              <div className=" alert alert-danger">{error.message}</div>
+            ))}
+            {error.length > 0 ? (
+              <div className=" alert alert-danger">{error}</div>
+            ) : succuss.length > 0 ? (
+              <div className=" alert alert-success">{succuss}</div>
+            ) : (
+              ""
+            )}
+            <label htmlFor="first_name">First Name:</label>
+            <input
+              onChange={getUserData}
+              type="text"
+              id="first_name"
+              name="first_name"
+              className=" form-control mb-2"
+            />
 
-                    <label htmlFor="last-name">Last Name:</label>
-                    <input
-                        onChange={getUserData}
-                        type="text"
-                        id="last-name"
-                        name="last-name"
-                        className=" form-control mb-2"
-                    />
-                    <label htmlFor="age">Age:</label>
-                    <input
-                        onChange={getUserData}
-                        type="number"
-                        id="age"
-                        name="age"
-                        className=" form-control mb-2"
-                    />
+            <label htmlFor="last-name">Last Name:</label>
+            <input
+              onChange={getUserData}
+              type="text"
+              id="last_name"
+              name="last_name"
+              className=" form-control mb-2"
+            />
+            <label htmlFor="age">Age:</label>
+            <input
+              onChange={getUserData}
+              type="number"
+              id="age"
+              name="age"
+              className=" form-control mb-2"
+            />
 
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        onChange={getUserData}
-                        type="email"
-                        id="email"
-                        name="email"
-                        className=" form-control mb-2"
-                    />
+            <label htmlFor="email">Email:</label>
+            <input
+              onChange={getUserData}
+              type="email"
+              id="email"
+              name="email"
+              className=" form-control mb-2"
+            />
 
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        onChange={getUserData}
-                        type="password"
-                        id="password"
-                        name="password"
-                        className=" form-control mb-2"
-                    />
-                    <button type="sumbit" className=" btn btn-outline-info">
-                        Register
-                    </button>
-                </form>
-            </div>
-        </>
+            <label htmlFor="password">Password:</label>
+            <input
+              onChange={getUserData}
+              type="password"
+              id="password"
+              name="password"
+              className=" form-control mb-2"
+            />
+            <button type="sumbit" className=" btn btn-outline-info">
+              {isLodaing === true ? (
+                <i className="fa-solid fa-spinner fa-spin-pulse fa-spin-reverse"></i>
+              ) : (
+                "Register"
+              )}
+            </button>
+          </form>
+        </div>
+      </>
     );
 }
