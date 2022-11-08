242518a7e447c169071af30f2e37da9bdebc632e
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 14:41:06 +0200

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
                 </Link>
               </li>
             </ul>
-            <ul className="navbar-nav  mb-2 mb-lg-0">
+            <ul className="navbar-nav d-flex mb-2 mb-lg-0">
               <li className="nav-item d-flex align-items-center">
                 <i className="fab mx-2 fa-facebook"></i>
                 <i className="fab mx-2 fa-twitter"></i>
