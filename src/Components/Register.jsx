596929eb118680d05859f0acdddd5da3e1c31e64
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 14:54:54 +0200

    update

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Register() {
   return (
       <>
-          
+          <div className=' w-75 mx-auto'>
+              
+          <form action="">
+              
+        </form>
+
+          </div>
     </>
   )
 }
