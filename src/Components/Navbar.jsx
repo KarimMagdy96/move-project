233fee2c4d5c18130399a3c3a0b319350d59d78c
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 14:06:07 +0200

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -33,6 +33,9 @@ export default function Navbar() {
       </ul>
       <ul className="navbar-nav  mb-2 mb-lg-0">
         <li className="nav-item">
+         
+        </li>
+        <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="register">Register</Link>
         </li>
         <li className="nav-item">
