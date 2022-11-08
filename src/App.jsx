5ecf738095e48b1595998884ec9074fbfea5ccc6
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 13:50:52 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ import Home from './Components/Home';
 import Login from './Components/Login';
 import Move from './Components/Move';
 import Navbar from './Components/Navbar';
+import About from './Components/About';
+import Contacts from './Components/Contacts';
+
+
 
 function App() {
  
@@ -21,6 +25,8 @@ function App() {
         <Route path='people' element={< People />} /> 
         <Route path='home'element={<Home />} /> 
         <Route path='login'element={<Login/>} /> 
+        <Route path='move' element={<Move />} /> 
+        <Route path='about' element={<Move />} /> 
         <Route path='move' element={<Move />} /> 
         <Route path='*' element={<Notfound/>}/>
       </Routes>
