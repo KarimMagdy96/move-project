ef00054ed58db768c3b07fa4e4fe9915bf784891
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-08 15:08:57 +0200

    update

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -7,7 +7,19 @@ export default function Register() {
         <h2>Register Now</h2>
         <form>
           <label htmlFor="first_name">First Name:</label>
-          <input type="text" id="first_name" name="first_name" className=' form-control mb-2' />
+                  <input type="text" id="first_name" name="first_name" className=' form-control mb-2' />
+                  
+          <label htmlFor="last-name">Last Name:</label>
+                  <input type="text" id="last-name" name="last-name" className=' form-control mb-2' />
+                  
+          <label htmlFor="email">Email:</label>
+                  <input type="email" id="email" name="email" className=' form-control mb-2' />
+                  
+          <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" name="password" className=' form-control mb-2' />
+                  
+          <label htmlFor="age">Age:</label>
+          <input type="text" id="age" name="age" className=' form-control mb-2' />
         </form>
       </div>
     </>
