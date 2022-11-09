7264fbbe4fc03d7afe4636804fe60f38a8a18bcf
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-09 18:43:31 +0200

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-export default function Navbar() {
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Navbar({ userdata }) {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
@@ -21,48 +21,66 @@ export default function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="home">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="tv">
-                  TV
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="people"
-                >
-                  People
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="move">
-                  Move
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="about"
-                >
-                  about
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="contacts"
-                >
-                  Contacts
-                </Link>
-              </li>
+              {userdata ? (
+                <>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="home"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="tv"
+                    >
+                      TV
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="people"
+                    >
+                      People
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="move"
+                    >
+                      Move
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="about"
+                    >
+                      about
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="contacts"
+                    >
+                      Contacts
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                ""
+              )}
             </ul>
             <ul className="navbar-nav d-flex mb-2 mb-lg-0">
               <li className="nav-item d-flex order-last order-lg-first align-items-center">
@@ -72,29 +90,34 @@ export default function Navbar() {
                 <i className="fab mx-2 fa-spotify"></i>
                 <i className="fab mx-2 fa-soundcloud"></i>
               </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="register"
-                >
-                  Register
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="login"
-                >
-                  LogIn
-                </Link>
-              </li>
-              <li className="nav-item">
-                <span className="nav-link active" aria-current="page">
-                  Logout
-                </span>
-              </li>
+              {userdata ? (
+                <li className="nav-item">
+                  <span className="nav-link active" aria-current="page">
+                    Logout
+                  </span>
+                </li>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="register"
+                    >
+                      Register
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active"
+                      aria-current="page"
+                      to="login"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
