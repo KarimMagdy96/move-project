39e9a64e60fe25fd2927fb4188f68f3d72e0e4c0
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-10 16:21:59 +0200

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 export default function Home() {
+  const [trendingMoves, setTrendingMovies] = useState([])
+  const [trendingTv, setTrendingTv] = useState([])
+  const [trendingPeople, setTrendingPeople] = useState([])
+
+ async function getMovies(mediaType,callback){
+   let { data } = await axios.get(
+     `https://api.themoviedb.org/3/trending/${mediaType}/day?api_key=fd3c31e2d7a54303dc08756b66824aef`
+   );
+    callback(data.results)
+  }
   return (
     <div>Home</div>
   )
